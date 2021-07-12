4485fc151411aeffaab067d4c466d385a9f30a45
Ryan Atkinson <mail@ryanatkn.com>
2021-07-12 08:43:58 -0500

    update gro and remove sveltekit dev switch for base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import static_adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV !== 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
 	kit: {
 		adapter: static_adapter(),
 		target: '#svelte',
-		paths: {base: dev ? '' : '/mirror-twin-sveltekit-vite'},
+		paths: {base: '/mirror-twin-sveltekit-vite'},
 		files: {assets: 'src/static'},
 		vite: {
 			ssr: {
